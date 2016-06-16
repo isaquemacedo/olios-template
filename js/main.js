024281323efe2c2d4ec5db0c85a27d6c30aa7ead
Isaque Macedo <isaque.mace@gmail.com>
2016-06-16 14:36:36 -0300

    adicionado arquivos do projeto]

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+  console.log('->', 'app');
+});
+
+$(function() {
+  console.log('->', 'carousel');
+});
+
+$(function() {
+  console.log('->', 'tab');
+});
+
+$(function() {
+  console.log('->', 'validation');
+});
