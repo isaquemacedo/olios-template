53f681de5bd694ba8f446ff9a64711217dc7cf4f
Isaque Macedo <isaque.mace@gmail.com>
2016-06-24 14:36:28 -0300

    estrutura da home com slider feita e icone do hamburguer em svg

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
-$(".btn-menu-drop").click(function() {
-  console.log("OK");
-  $("#container-menu-drop-down").toggle( "slow", function() { } );
-});
-
-$("#wrapper").click(function () {
-  $("#container-menu-drop-down").hide("slow", function() {
-    $(this).css("display", "none");
+//Função toggle do botão (hamburguer) fixo
+function menuDrop(a){
+  $(a).click(function() {
+    console.log("OK");
+    $("#container-menu-drop-down").toggle( "slow", function() { } );
   });
+
+  $("#wrapper").click(function () {
+    $("#container-menu-drop-down").hide("slow", function() {
+      $(this).css("display", "none");
+    });
+  });
+}
+
+// FUNÇÕES DO SLIDE
+//Variáveis
+containerWidth = 0;
+valuePin = 0;
+
+//FUNÇÕES
+//Função que seta a largura do container do slide
+function setWidthCarrousel(){
+  amount_item = $(".slider-itens").length;
+  itemWidth = $(".slider-itens").width();
+  $(".container-slider").width(itemWidth * amount_item);
+}
+
+//Função que retorna o indice do pin
+function slider_value(){
+  $('.slider-pin').click(function() {
+    setWidthCarrousel();
+    value = $(this).text();
+    slide_drag();
+  });
+}
+
+function slide_drag(){
+  var left_value = itemWidth * value;
+  $(".container-slider").animate({"left" : "-" + left_value + "px"});
+}
+
+//Ready da página
+$(function() {
+  menuDrop(".btn-menu-drop");
+  setWidthCarrousel();
+  slider_value();
 });
