4cedda643da523499d8b8acd9ed01c871934de1c
Isaque Macedo <isaque.mace@gmail.com>
2016-06-27 17:50:02 -0300

    ajustes na responsivdade dos itens no carrousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Função toggle do botão (hamburguer) fixo
-function menuDrop(a){
+function menuDrop(a) {
   $(a).click(function() {
     console.log("OK");
     $("#container-menu-drop-down").toggle( "slow", function() { } );
@@ -15,29 +15,41 @@ function menuDrop(a){
 // FUNÇÕES DO SLIDE
 //Variáveis
 containerWidth = 0;
-valuePin = 0;
 
 //FUNÇÕES
 //Função que seta a largura do container do slide
-function setWidthCarrousel(){
+function setWidthCarrousel() {
   amount_item = $(".slider-itens").length;
   itemWidth = $(".slider-itens").width();
   $(".container-slider").width(itemWidth * amount_item);
 }
 
 //Função que retorna o indice do pin
-function slider_value(){
+function slider_value() {
   $('.slider-pin').click(function() {
     setWidthCarrousel();
     value = $(this).text();
+    $(this).parent("container-pins").find("slider-pin").removeClass( "slider-pin-hover");
+    $(this).toggleClass( "slider-pin-hover");
     slide_drag();
   });
 }
 
-function slide_drag(){
+//Função que movimenta o slide
+function slide_drag() {
   var left_value = itemWidth * value;
   $(".container-slider").animate({"left" : "-" + left_value + "px"});
 }
+
+// function pinActive(pos){
+//   if (pos === "0") {
+//     $(pos).addClass("slider-pin-hover");
+//     // console.log("if");
+//   } else {
+//     $(pos).removeClass("slider-pin-hover");
+//     // console.log("else");
+//   }
+// }
 
 //Ready da página
 $(function() {
